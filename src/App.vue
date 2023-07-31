0a67c675c3fd0d19a2642816dab43cbdd24a6884
Ivan Palii <vanopaliy@gmail.com>
2023-08-01 07:50:28 +0900

    task complete

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,97 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import ToDoItem from "./components/ToDoItem.vue";
+import axios from 'axios';
+import {ref} from "vue";
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com/'
+//https://jsonplaceholder.typicode.com/users/4/todos
+
+const todoList = ref([])
+const currentUser = ref(5)
+
+function getToDoList() {
+	console.log('here')
+	axios.get(BASE_URL + 'users/' + currentUser.value + '/todos').then(
+		function (response) {
+			console.log(response.data)
+			todoList.value = []
+			response.data.forEach(item => todoList.value.push(item))
+		},
+		function (error) {
+			console.log(error)
+		}
+	)
+
+}
+
+getToDoList()
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+	<div class="wrapper">
+		<header class="header">
+			<h1 class="header__title">ToDo list</h1>
+		</header>
+		<main class="main">
+			<div class="main__todo-container todo">
+				<ToDoItem
+					v-for="item in todoList"
+					class="todo__item"
+					:class="{'todo__item_done': item.completed,
+					'todo__item_pending': !item.completed}"
+					:todo-item="item"
+				/>
+			</div>
+		</main>
+	</div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
+<style
+	scoped
+	lang="scss"
+>
+.wrapper {
+	padding: 15px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.header {
+	// .header__title
+	&__title {
+		font-size: 46px;
+		font-weight: 700;
+	}
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.main {
+	margin: 20px;
+	// .main__todo-container
+	&__todo-container {
+		height: 800px;
+		overflow-y: scroll;
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+	}
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.todo {
+	// .todo__item
+	&__item {
+		height: 50px;
+		border: 3px solid;
+		border-radius: 15px;
+		display: flex;
+		align-items: center;
+		padding: 5px;
+		&_pending {
+			background-color: #ffc662;
+			border-color: orangered;
+		}
+
+		&_done {
+			background-color: lightgreen;
+			border-color: darkgreen;
+		}
+		&:not(:last-child){
+			margin-bottom: 2px;
+		}
+	}
+
 }
 </style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<label :for="'todoStatus'+todoItem.id"  class="todo-item__text"> {{ todoItem.title }}</label>
+		<input
+			:id="'todoStatus'+todoItem.id"
+			type="checkbox"
+			:checked="todoItem.completed"
+			@change="changeState"
+		>
+	</div>
+</template>
+<script setup>
+const props = defineProps({
+	todoItem: {
+		type: Object,
+		required: true
+	}
+})
+
+function changeState() {
+	props.todoItem.completed = !props.todoItem.completed
+}
+</script>
+<style scoped>
+input {
+	margin-left: 20px;
+	display: block;
+	width: 25px;
+	height: 25px;
+}
+</style>
